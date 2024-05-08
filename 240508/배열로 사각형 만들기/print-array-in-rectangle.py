c7600c9f72a60fcf39f9f7e9be4868cffe14b4c5
Hanseo Lee <95961223+beaver-zip@users.noreply.github.com>
2024-05-08 15:20:13 +0900

    Title: 배열로 사각형 만들기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240508/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -0,0 +1,10 @@
+mat = [[0 for _ in range(5)] for _ in range(5)]
+mat[0] = [1] * 5
+for i in range(1, 5):
+    mat[i][0] = 1
+for i in range(1, 5):
+    for j in range(1, 5):
+        mat[i][j] = mat[i][j-1] + mat[i-1][j]
+
+for row in mat:
+    print(*row)
