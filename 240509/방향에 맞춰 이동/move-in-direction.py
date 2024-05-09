e62f0b2bd01d372531b36e7ba5bd63d694989a03
Hanseo Lee <95961223+beaver-zip@users.noreply.github.com>
2024-05-09 11:42:58 +0900

    Title: 방향에 맞춰 이동, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240509/방향에 맞춰 이동/move-in-direction.py
+++ 240509/방향에 맞춰 이동/move-in-direction.py
@@ -1,20 +1,11 @@
-dx, dy = [-1, 0, 0, 1], [0, -1, 1, 0]
+dx, dy = {'W': -1, 'S': 0, 'N': 0, 'E':1}, {'W': 0, 'S': -1, 'N': 1, 'E': 0}
 x, y = 0, 0
 
 for _ in range(int(input())):
     dir_char, n = input().split()
     n = int(n)
-    if dir_char == 'W':
-        x += dx[0] * n
-        y += dy[0] * n
-    elif dir_char == 'S':
-        x += dx[1] * n
-        y += dy[1] * n
-    elif dir_char == 'N':
-        x += dx[2] * n
-        y += dy[2] * n
-    elif dir_char == 'E':
-        x += dx[3] * n
-        y += dy[3] * n
+
+    x += dx[dir_char] * n
+    y += dy[dir_char] * n
 
 print(x, y)
